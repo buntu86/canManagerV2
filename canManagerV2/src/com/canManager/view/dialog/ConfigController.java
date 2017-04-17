8ccd3f41a3da0f9471f406fc311d5917ee909c78
adiHome <>
2017-04-17 21:06:51 +0200

    - fileChooser config OK

--- canManagerV2/src/com/canManager/view/dialog/ConfigController.java
+++ canManagerV2/src/com/canManager/view/dialog/ConfigController.java
@@ -2,8 +2,11 @@ package com.canManager.view.dialog;
 
 import com.canManager.utils.Log;
 import com.canManager.utils.Config;
+import java.io.File;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 
@@ -31,5 +34,24 @@ public class ConfigController {
         Config.setCatalogDirectory(label_pathCatalogDirectory.getText());
         stage.close();
         Log.msg(0, "Dialog config : save");
+    }
+    
+    @FXML
+    private void handleFileChooser(){
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Choix du répertoire");
+        File defaultDirectory = new File(label_pathCatalogDirectory.getText());
+        if(defaultDirectory.isDirectory())
+            chooser.setInitialDirectory(defaultDirectory);
+        else
+            chooser.initialDirectoryProperty();
+        File selectedDirectory = chooser.showDialog(null);
+        if(selectedDirectory != null)
+        {   
+            label_pathCatalogDirectory.setText(selectedDirectory.toString());
+        }   
+        
+        else
+            System.out.println("Selection du répertoire annulé"); 
     }
 }
